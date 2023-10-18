7b81ec5f2458c1bd863f4316ed5cc9658c8f7a32
lianshufeng <251708339@qq.com>
2023-10-19 01:43:17 +0800

    优化代码

--- src/main/java/com/github/jrebel/core/controller/IndexController.java
+++ src/main/java/com/github/jrebel/core/controller/IndexController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
 import java.util.HashMap;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,13 +15,16 @@ public class IndexController {
 
     @RequestMapping({"/", ""})
     public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
-        String licenseUrl =   System.getenv().getOrDefault("licenseUrl","//" + request.getServerName() + (request.getServerPort() == 80 ? "" : ":" + request.getServerPort()));
+        String licenseUrl = System.getenv().getOrDefault("licenseUrl", request.getServerName() + (request.getServerPort() == 80 ? "" : ":" + request.getServerPort()));
+        String protocol = System.getenv().getOrDefault("protocol", "http://");
+
+
         return new ModelAndView("index", new HashMap<String, Object>() {{
             put("licenseUrl", licenseUrl);
+            put("protocol", protocol);
             put("uuid", UUID.randomUUID().toString());
         }});
     }
-
 
 
 }
