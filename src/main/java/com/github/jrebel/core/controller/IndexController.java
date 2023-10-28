4a636c959065a5e8184b2a9c300a73dec1beb8f1
lianshufeng <251708339@qq.com>
2023-10-29 02:26:51 +0800

    修改默认页面的跳转

--- src/main/java/com/github/jrebel/core/controller/IndexController.java
+++ src/main/java/com/github/jrebel/core/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.github.jrebel.core.controller;
 import com.github.jrebel.core.util.RandomUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,17 +17,19 @@ import java.util.UUID;
 public class IndexController {
 
     @RequestMapping({"/", ""})
-    public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
-        String licenseUrl = System.getenv().getOrDefault("licenseUrl", request.getServerName() + (request.getServerPort() == 80 ? "" : ":" + request.getServerPort()));
-        String protocol = System.getenv().getOrDefault("protocol", "http://");
-
-
-        return new ModelAndView("index", new HashMap<String, Object>() {{
-            put("licenseUrl", licenseUrl);
-            put("protocol", protocol);
-            put("uuid", UUID.randomUUID().toString());
-            put("mail", "%s@qq.com".formatted(RandomUtil.next(10000, 999999999)));
-        }});
+    @SneakyThrows
+    public void index(HttpServletRequest request, HttpServletResponse response) {
+//        String licenseUrl = System.getenv().getOrDefault("licenseUrl", request.getServerName() + (request.getServerPort() == 80 ? "" : ":" + request.getServerPort()));
+//        String protocol = System.getenv().getOrDefault("protocol", "http://");
+//
+//
+//        return new ModelAndView("index", new HashMap<String, Object>() {{
+//            put("licenseUrl", licenseUrl);
+//            put("protocol", protocol);
+//            put("uuid", UUID.randomUUID().toString());
+//            put("mail", "%s@qq.com".formatted(RandomUtil.next(10000, 999999999)));
+//        }});
+        response.sendRedirect("//www.jpy.wang/page/jrebel.html");
     }
 
 
